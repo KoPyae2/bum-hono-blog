5af2eab4eb1e15b5dceb39bd89d9153a16afde38
chico <chico@gmaiil.com>
2024-10-28 11:46:44 +0800

    setup db

---
+++ server/adapter.ts
@@ -0,0 +1,30 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import postgres from "postgres";
+import { z } from "zod";
+import { sessionTable, userTable } from "./db/schemas/auth";
+
+
+const EnvSchema = z.object({
+  DATABASE_URL: z.string().url(),
+});
+
+const processEnv = EnvSchema.parse(process.env);
+
+const queryClient = postgres(processEnv.DATABASE_URL);
+export const db = drizzle(queryClient, {
+  schema: {
+    user: userTable,
+    session: sessionTable
+  },
+});
+
+export const adapter = new DrizzlePostgreSQLAdapter(
+  db,
+  sessionTable,
+  userTable,
+);
+
+// const result = await db.execute('select 1');
+// console.log(result);
